545114333d0b4fd28f5d5eefb787b7d069bee4ae
nelyflores <nely.afloresr@gmail.com>
2017-08-16 05:52:40 +0000

    agrega un mapa

--- detalleproducto.js
+++ detalleproducto.js
@@ -2,3 +2,10 @@ $(document).ready(function(){
      $('.carousel').carousel();
        $('.collapsible').collapsible();
    });
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
